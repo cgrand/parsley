1d7ad886ba86f1242a6a0627b2a90523e73b001a
Christophe Grand <christophe@cgrand.net>
2009-09-10 02:18:45 +0200

    design in flux

--- src/net/cgrand/parsley.clj
+++ src/net/cgrand/parsley.clj
@@ -1,68 +1,96 @@
 (ns net.cgrand.parsley)
 
-(defn len [spans] 
-  (reduce + 0 (map :length spans)))
+;; a parser is a function that takes a chunk of text (a String) and returns
+;; a coll of [events n conts]
+
+(defmulti interpret-cont (fn [_ cont _] (type cont)))
+
+;; ref
+(defmethod interpret-cont clojure.lang.Keyword [grammar kw s]
+  [[nil s [(grammar kw)]]])
+
+;; alternative
+(defmethod interpret-cont clojure.lang.IPersistentSet [_ conts s]
+  (map (fn [cont] [nil s [cont]]) conts)) 
   
-(defn span [class contents]
-  (if (string? contents)
-    {:class class :contents nil :text contents :length (count contents)}
-    {:class class :contents contents :length (len contents)}))
+;; sequence
+(defmethod interpret-cont clojure.lang.IPersistentVector [_ conts s]
+  [[nil s conts]])
+
+;; terminal
+(defmethod interpret-cont String [_ #^String terminal #^String s]
+  (let [n (count terminal)]
+    (cond 
+      (< (count s) n)
+        [[nil nil [{:buffer s :cont terminal}]]]
+      (.startsWith s terminal)
+        [[[terminal] (subs s (count terminal)) nil]])))
+
+;; regex
+(defmethod interpret-cont java.util.regex.Pattern 
+ [_ #^java.util.regex.Pattern pattern #^String s]
+  (let [matcher (.matcher pattern s)
+        found (.lookingAt matcher)]
+    (cond
+      (.hitEnd matcher) 
+        [[nil nil [{:buffer s :cont pattern}]]]
+      found 
+        [[[(.group matcher)] (subs s (.end matcher)) nil]])))
     
-(defn alt 
- [& parsers]
-  (fn [s] 
-    (mapcat #(% s) parsers)))
+;; pass
+(defmethod interpret-cont nil [_ _ s]
+  [[nil s nil]])
 
-(defn cat
- [& parsers]
-  (fn [s]
-    (when-let [[parser & parsers] (seq parsers)]
-      (for [[spans n conts] (parser s)]
-        [spans n (concat conts parsers)]))))
-  
-(defn string-parser [class text]
-  (let [len (count text)
-        ok [[[(span class text)] len nil]]
-        parser 
-          (fn parser [text]
-            (fn [s]
-                (let [n (count s)]
-                  (if (<= len n)
-                    (when (= text (subs s 0 len))
-                      ok)
-                    (when (= s (subs text 0 n))
-                      (parser (subs text n)))))))]
-    (parser text)))
+(defmethod interpret-cont clojure.lang.IPersistentMap 
+ [grammar {:keys [buffer cont]} s]
+  [[nil (str buffer s) [cont]]])
 
-(defn- start-span [class] [:start-span class])  
+;; fn
+(defmethod interpret-cont clojure.lang.Fn [grammar cont s]
+  (cont s))
 
-(defn span-parser [class parser]
-  (let [end-ops [[:end-span class]]
-        start-op (start-span class)
-        startify (fn [parser]
-                   (fn [s]
-                     (for [[ops n conts] (parser s)]
-                       [(cons start-op ops) n conts])))
-        endify (fn endify [parser]
-                 (fn [s]
-                   (for [[ops n conts] (parser s)]
-                     (if (seq conts)
-                       [ops n (concat (butlast conts) [(endify (last conts))])] 
-                       [(concat ops end-ops) n nil]))))]
-    (-> parser startify endify)))
+(defn- interpreter-step1 [f grammar s [_ acc conts]]
+  (let [k conts
+        step1 (fn [s acc conts]
+                (if-let [[cont & next-conts] (seq conts)]
+                  (mapcat (fn [[ops s conts]]
+                            (let [acc (reduce f acc ops)
+                                  conts (concat conts next-conts)]
+                                (if s
+                                  (step1 s acc conts)  
+                                  [[k acc conts]])))
+                    (interpret-cont grammar cont s))
+                  (when (empty? s)
+                    [[k acc nil]])))]
+    (step1 s acc conts)))
 
-(def pass (constantly [[nil 0 nil]]))
+(defn- interpreter-step [f grammar states s]
+  (mapcat (partial interpreter-step1 f grammar s) states))
 
-(defn- Y [f]
-  (let [g (memoize (fn [self] (f #((self self) %))))]
-    (g g)))    
+(defn- start-span [class] 
+  (let [ops [[:start-span class]]]
+    (fn [s] [[ops s nil]])))  
 
-(defn repeat-parser [parser]
-  (Y (fn [self]
-       (alt (cat parser self) pass))))
+(defn- end-span [class] 
+  (let [ops [[:end-span class]]]
+    (fn [s] [[ops s nil]])))  
+
+(defn- zero-or-more [parser]
+  (fn self [s] [[nil s nil] [nil s [parser self]]])) 
 
 ;;;;;;;;;;;;;;;;;;;;;;
+(defn- group-reduce 
+ [k f seed coll]
+  (persistent! (reduce #(let [key (k %2)]
+                          (assoc! %1 key (f (%1 key seed) %2)))
+                 (transient {}) coll)))
 
+(defn stitch [op states-a states-b]
+  (let [states-b-by-k (group-reduce first conj #{} states-b)]
+    (mapcat (fn [[k a conts]]
+              (for [[_ b conts] (states-b-by-k k)] [k (op a b)])) states-a)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn- step-stack [stack op]
   (if (vector? op)
     (condp = (first op)
@@ -73,28 +101,16 @@
                     (cons (conj top span) stack))))
     (let [[top & stack] stack]
       (cons (conj top op) stack))))
+
+(defn len [events] 
+  (reduce + 0 (map #(:length % 0) events)))
+
+(defn span [class contents]
+  (if (string? contents)
+    {:class class :contents nil :text contents :length (count contents)}
+    {:class class :contents contents :length (len contents)}))   
+
       
-(defn- consume
- [conts s]
-  (when-let [[cont & conts] (seq conts)]
-    (mapcat (fn [[ops n new-conts]]
-              (let [conts (concat new-conts conts)]
-                (if (< n (count s))
-                  (for [[o c] (consume conts (subs s n))]
-                    [(concat ops o) c])
-                  [[ops conts]])))
-      (cont s))))
-
-(defn step
- "Consumes s and returns a seq of states." 
- [states s]
-  (if (seq s)
-    (letfn [(step1 [[stack conts]]
-              (for [[ops conts] (consume conts s)]
-                [(reduce step-stack stack ops) conts]))]
-      (mapcat step1 states))
-    states)) 
-
 (comment
 (defgrammar
  [list ["(" expr * ")"]
@@ -106,32 +122,23 @@
  ; :whitespace XXX
  :main expr))
 
-(defmulti compile-rhs (fn [grammar-sym x] (type x)))
+(defmulti compile-spec type x)
 
 ;; a run
-
-(defmethod compile-rhs clojure.lang.IPersistentVector
- [grammar-sym v]
-  (let [v (reduce (fn [v x]
-                    (cond
-                      (= '* x) (conj (pop v) `(repeat-parser ~(peek v)))
-                      (= '+ x) (conj (pop v) `(cat ~(peek v) (repeat-parser ~(peek v))))
-                      :else (conj v (compile-rhs grammar-sym x)))) [] v)]
-    `(cat ~@v)))
+(defmethod compile-spec clojure.lang.IPersistentVector [v]
+  (reduce (fn [v x]
+    (cond
+      (= '* x) (conj (pop v) (zero-or-more (peek v)))
+      (= '+ x) (conj v (zero-or-more (peek v)))
+      (= '? x) (conj (pop v) #{(peek v) nil})
+      :else (conj v x))) [] (map compile-spec v)))
 
 ;; an alternative
-(defmethod compile-rhs clojure.lang.IPersistentSet
- [grammar-sym s]
-  `(alt ~@(map (partial compile-rhs grammar-sym) s)))
-
-;; a literal string
-(defmethod compile-rhs String
- [grammar-sym s]
-  `(string-parser nil ~s))
+(defmethod compile-spec clojure.lang.IPersistentSet [s]
+  `#{~@(map compile-spec s}))
 
 ;; else
-(defmethod compile-rhs :default
- [grammar-sym x]
+(defmethod compile-spec :default [x]
   x)
 
 (defn grammar* [grammar main-rule]
@@ -139,16 +146,17 @@
     [['([]) [main]]])) 
 
 (defn- compile-rule [grammar-sym [name rhs]]
-  `[~(keyword name) (span-parser ~(keyword name) ~(compile-rhs grammar-sym rhs))]) 
+  `[~(keyword name) (span-parser ~(keyword name) ~(compile-spec grammar-sym rhs))]) 
       
 (defn- compile-rec-rule [grammar-sym [sym _]] 
   `(~sym [s#] 
      ((~grammar-sym ~(keyword sym)) s#)))
   
 (defmacro grammar [rules & options]
-  (let [rules (partition 2 rules) 
-        {:keys [main] :or {main 'main}} (apply hash-map options)
-        grammar-sym (gensym "grammar")]
+  (let [rules (reduce (fn [rules [k v]] (assoc rules k (compile-spec v))) 
+                {} (partition 2 rules))
+        {:keys [main]} (apply hash-map options)
+        main (or main (if (= 1 (count rules)) (key (first rules)) :main))] 
     `(grammar* 
        (fn [~grammar-sym]
          (letfn [~@(map (partial compile-rec-rule grammar-sym) rules)]
@@ -158,7 +166,7 @@
 (def g (grammar [expr #{"-" ["(" expr * ")"]}] :main expr))
 
 (def a (grammar
-         {:sum #{:prod [:prod "+" :sum]}
-          :prod #{:num [:num "*" :prod]}
-          :num #{"1" "0" ["(" :sum ")"]}}
-         :main :sum))
+         {sum #{:prod [:prod "+" :sum]}
+          prod #{:num [:num "*" :prod]}
+          num #{"1" "0" ["(" :sum ")"]}}
+         :main sum))
