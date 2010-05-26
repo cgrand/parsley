8ffbd03604f9b7f17ee3690286e68447ec63fb12
Christophe Grand <christophe@cgrand.net>
2010-05-26 02:36:48 +0200

    switch to protocols and remove macros

--- src/net/cgrand/parsley.clj
+++ src/net/cgrand/parsley.clj
@@ -25,46 +25,91 @@
 ;; * keywords (non-terminal)
 ;; * lists (special-ops: follow restrictions, rejects)
 
+(defprotocol RuleFragment
+  (collect [fragment token-mode top-rulename])
+  (develop [fragment rewrite space]))
+
+(defrecord Alt [items]
+  RuleFragment
+    (collect [this token-mode top-rulename]
+      (mapcat #(collect % token-mode top-rulename) items))
+    (develop [this rewrite space]
+      (mapcat #(rewrite % space) items)))
+
+(defrecord Seq [items]
+  RuleFragment
+    (collect [this token-mode top-rulename]
+      (mapcat #(collect % token-mode top-rulename) items))
+    (develop [this rewrite space]
+      (reduce #(for [x (rewrite %2 space) xs %1] 
+                 (concat x (and space (seq x) (seq xs) [space]) xs))
+        [()] (rseq items))))
+
+(defrecord Token [item]
+  RuleFragment
+    (collect [this token-mode top-rulename]
+      (collect item true top-rulename))
+    (develop [this rewrite space]
+      (rewrite item nil)))
+
+(defrecord Repeat+ [item]
+  RuleFragment
+    (collect [this token-mode top-rulename]
+      (let [kw (keyword (gensym (str top-rulename "_repeat+_")))
+            alt (Alt. [(Seq. [kw item]) item])]
+        (cons [this kw (if token-mode (Token. alt) alt)] 
+          (collect item token-mode top-rulename)))))
+
+(defrecord Follow [item negative]
+  RuleFragment
+    (collect [this token-mode top-rulename]
+      (collect item token-mode top-rulename))
+    (develop [this rewrite space]
+      [[{:follow1 (set (rewrite item space))
+         :complement negative}]]))
+
 ;; 1. compile-spec turns a sugar-heavy grammar in a sugar-free grammar  
 (defmulti #^{:private true} compile-spec type)
 
 ;; a vector denotes a sequence, supports postfix operators + ? and *
 (defmethod compile-spec clojure.lang.IPersistentVector [specs]
-  (reduce #(condp = %2 
-             '* (conj (pop %1) [:alt [:seq] (vector :repeat+ (peek %1))]) 
-             '+ (conj (pop %1) (vector :repeat+ (peek %1)))
-             '? (conj (pop %1) [:alt [:seq] (peek %1)])
-             (conj %1 (compile-spec %2))) [:seq] specs))
+  (Seq. 
+    (reduce #(condp = %2 
+             :* (conj (pop %1) (Alt. [(Seq. []) (Repeat+. (peek %1))])) 
+             :+ (conj (pop %1) (Repeat+. (peek %1)))
+             :? (conj (pop %1) (Alt. [(Seq. []) (peek %1)]))
+             (conj %1 (compile-spec %2))) [] specs)))
+  
 
 ;; a set denotes an alternative
 (defmethod compile-spec clojure.lang.IPersistentSet [s]
-  (into [:alt] (map compile-spec s)))
+  (Alt. (map compile-spec s)))
 
 ;; a ref to another rule: add support for + ? or * suffixes
 (defmethod compile-spec clojure.lang.Keyword [kw]
   (if-let [[_ base suffix] (re-matches #"(.*?)([+*?])" (name kw))] 
-    (compile-spec [(keyword base) (symbol suffix)])
+    (compile-spec [(keyword base) (keyword suffix)])
     kw))
 
 ;; else pass through
 (defmethod compile-spec :default [x]
   x)
 
-(defmacro spec [& xs]
+(defn spec [& xs]
   (compile-spec (vec xs)))
 
 ;; DSL utils
-(defmacro token [& specs]
-  `[:token (spec ~@specs)])
+(defn token [& specs]
+  (Token. (apply spec specs)))
 
-(defmacro ?= [& specs]
-  `[:follow (spec ~@specs)])
+(defn ?= [& specs]
+  (Follow. (apply spec specs) false))
 
-(defmacro ?! [& specs]
-  `[:not-follow (spec ~@specs)])
+(defn ?! [& specs]
+  (Follow. (apply spec specs) true))
 
 (defn any-of [s]
-  (into [:alt] s))
+  (zipmap s s))
 
 (defn none-of [& cs]
   (let [cps (map int (sort (apply str cs)))]
@@ -73,41 +118,39 @@
 
 (def any-char {0 core/*max*})
 
-(def $ {-1 0})
+(def $ {-1 -1})
 
 ;; 2. collect new rules
-(defn- genkey [s]
-  (-> s gensym keyword))
-
-(def *current-rule*)
-
-(defn- genderived-key [s]
-  (genkey (str (if *current-rule* (name *current-rule*) "") s)))
-
-(defmulti collect-rules #(when (vector? %2) (first %2)))
-
-(defmethod collect-rules :repeat+ [token-mode v]
-  (let [kw (genderived-key "_repeat+_")
-        rule (second v)]
-    (cons [v kw [(if token-mode :token :seq) [:alt [:seq kw rule] rule]]] 
-      (collect-rules token-mode rule))))   
-
-(defmethod collect-rules :alt [token-mode v]
-  (mapcat (partial collect-rules token-mode) (rest v)))
-
-(defmethod collect-rules :seq [token-mode v]
-  (mapcat (partial collect-rules token-mode) (rest v)))
-  
-(defmethod collect-rules :token [token-mode v]
-  (collect-rules true (second v)))
-  
-(defmethod collect-rules :default [token-mode v]
-  nil)
+(extend-protocol RuleFragment
+  nil
+    (collect [this token-mode top-rulename]
+      nil)
+    (develop [this rewrite space]
+      [()])
+  String
+    (collect [this token-mode top-rulename]
+      nil)
+    (develop [this rewrite space]
+      [(map core/ranges this)])
+  Character
+    (collect [this token-mode top-rulename]
+      nil)
+    (develop [this rewrite space]
+      [[(core/ranges this)]])
+  clojure.lang.IPersistentMap 
+    (collect [this token-mode top-rulename]
+      nil)
+    (develop [this rewrite space]
+      [[(apply core/ranges this)]])
+  Object
+    (collect [this token-mode top-rulename]
+      nil)
+    (develop [this rewrite space]
+      [[this]]))
 
 (defn collect-new-rules [grammar]
-  (let [collected-rules (mapcat (fn [[k v]]
-                          (binding [*current-rule* k]
-                            (doall (collect-rules false v)))) grammar)
+  (let [collected-rules 
+         (mapcat (fn [[k v]] (collect v false (name k))) grammar)
         rewrites (into {} (for [[op k] collected-rules] [op k]))
         new-rules (set (vals rewrites))  
         grammar (into grammar (for [[_ k v] collected-rules
@@ -115,47 +158,14 @@
     [rewrites grammar]))
 
 ;; 3. develop-alts: 
-(defmulti develop-alts (fn [rewrites space v]
-                         (cond
-                           (rewrites v) :maybe-rewrite
-                           (vector? v) (first v)
-                           :else (type v)))
-  :default :maybe-rewrite)
-
-(defmethod develop-alts :seq [rewrites space v]  
-  (reduce #(for [x (develop-alts rewrites space %2) xs %1] 
-             (concat x (and space (seq x) (seq xs) [space]) xs))
-    [()] (rseq (subvec v 1))))
-
-(defmethod develop-alts :alt [rewrites space v]
-  (mapcat (partial develop-alts rewrites space) (rest v)))
-
-(defmethod develop-alts :token [rewrites _ v]
-  (develop-alts rewrites nil (second v)))
-  
-(defmethod develop-alts :follow [rewrites space v]
-  [[{:follow1 (set (develop-alts rewrites space (second v)))}]]) 
-
-(defmethod develop-alts :not-follow [rewrites space v]
-  [[{:follow1 (set (develop-alts rewrites space (second v)))
-     :complement true}]]) 
-
-(defmethod develop-alts :maybe-rewrite [rewrites _ x]
-  [[(rewrites x x)]])
-
-(defmethod develop-alts String [_ _ s]
-  [(map core/ranges s)])
-  
-(defmethod develop-alts Character [_ _ c]
-  [[(core/ranges c)]])
-  
-(defmethod develop-alts clojure.lang.IPersistentMap [_ _ m]
-  [[(apply core/ranges m)]])
-  
-(defn develop 
- ([grammar] (develop nil {} grammar))
- ([space rewrites grammar]
-  (into {} (for [[k v] grammar] [k (set (develop-alts rewrites space v))])))) 
+(defn normalize 
+ ([grammar] (normalize grammar nil {}))
+ ([grammar space rewrites]
+  (letfn [(helper [item space]
+            (if-let [rw (rewrites item)]
+              [[rw]]
+              (develop item helper space)))]
+    (into {} (for [[k v] grammar] [k (set (helper v space))])))))
 
 ;; 4. remove-empty-prods
 (defn- empty-prod? [prod]
@@ -194,38 +204,9 @@
       (for [[k prods] grammar]
         [k (-> prods (inline-prods empty-prods #{space}) set (disj [k]))]))))  
              
-(defn inline-empty-prods [space grammar]
+(defn inline-empty-prods [grammar space]
   (core/fix-point #(inline-empty-prods* % space) grammar)) 
 
-;; 5. remove-singletons
-(defn remove-singletons [protected? grammar]
-  (let [non-singletons (into {}
-                     (for [[k v] grammar
-                           :let [sv (if (protected? k) 
-                                      v 
-                                      (remove #(= 1 (count %)) v))]
-                           :when (seq sv)]
-                       [k sv]))
-        ;; singletons is a map of keywords to seqs of 1-item vectors
-        singletons (into {}
-                     (for [[k v] grammar
-                           :when (not (protected? k))
-                           :let [sv (filter #(= 1 (count %)) v)
-                                 sv (if (non-singletons k)
-                                      (cons [k] sv)
-                                      sv)]
-                           :when (seq sv)]
-                       [k sv]))
-        singletons (into {}
-                     (for [[k sv] singletons]
-                       [k (core/fix-point 
-                            (fn [sv] (set (mapcat #(singletons (first %) [%]) sv))) 
-                            sv)]))]
-    (into {}
-      (for [[k prods] non-singletons 
-            :let [prods (inline-prods prods singletons)]]
-        [k prods]))))
-       
 
 
 (defn- private? [kw]
@@ -234,39 +215,29 @@
       (keyword (subs s 0 (dec (count s)))))))
 
 ;;;;;;;;;;;
-(defmacro grammar [options-map & rules]
-  (if-not (map? options-map)
-    `(grammar {} ~options-map ~@rules)
-    (let [{:keys [main seed reducer stitch result] 
-           :or {seed `tree-seed 
-                reducer `tree-reducer 
-                stitch `tree-stitch
-                result `tree-result}} options-map 
-          rules (partition 2 rules)
-          public-rulenames (remove private? (map first rules))
-          main (or (:main options-map) (first public-rulenames))
-          public-rulenames (set public-rulenames)
-          space-name (when (options-map :space) (genkey "space__"))
-          main-name (genkey "main__")
-          grammar (into {main-name `(spec ~@(when space-name [space-name ]) 
-                                              ~main core/$)}
-                    (for [[name specs] rules]
-                      [(or (private? name) name) `(spec ~specs)]))
-          grammar (if space-name 
-                    (assoc grammar space-name `(token ~(options-map :space)))
-                    grammar)]
-    `[(->> ~grammar collect-new-rules 
-         (apply develop ~space-name) (inline-empty-prods ~space-name) 
-         #_(remove-singletons (conj ~public-rulenames ~main-name)))
-      ~main-name
-      ~public-rulenames]))) 
+(defn grammar [options-map & rules]
+  (let [[options-map rules] (if-not (map? options-map)
+                              [{} (cons options-map rules)]
+                              [options-map rules])
+        rules (partition 2 rules)
+        public-rulenames (remove private? (map first rules))
+        {:keys [main space] :or {main (first public-rulenames)}} options-map
+        public-rulenames (set public-rulenames)
+        grammar (into {::main (spec (when space ::space) main $)
+                       ::space (token space)}
+                  (for [[name specs] rules]
+                    [(or (private? name) name) (spec specs)]))
+        [rewrites grammar] (collect-new-rules grammar)
+        grammar (-> grammar (normalize (and space ::space) rewrites) 
+                  (inline-empty-prods ::space))]
+  [grammar ::main public-rulenames])) 
 
 (defn stepper [table]
   (fn [state s]
     (core/step state table s)))
 
-(defmacro parser [options-map & rules]
-  `(stepper (apply core/lr-table (grammar ~options-map ~@rules))))
+(defn parser [options-map & rules]
+  (stepper (apply core/lr-table (apply grammar options-map rules))))
 
 (comment
 (def table (apply lr-table 
