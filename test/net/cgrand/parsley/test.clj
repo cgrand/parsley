c863eac3b1e1e90469e2645ecae2df568a265615
Christophe Grand <christophe@cgrand.net>
2010-05-26 09:24:51 +0200

    oops, forgot to include the revised test grammar in the previous commit

--- test/net/cgrand/parsley/test.clj
+++ test/net/cgrand/parsley/test.clj
@@ -4,15 +4,15 @@
   (:use clojure.test))
 
 (def sexp 
-  (parser {:space [#{:whitespace :comment :discard}*]
+  (parser {:space [#{:whitespace :comment :discard}:*]
             :main :expr*}
     :expr- #{:atom :list :vector :set :map :string :regex
              :meta :deprecated-meta :quote 
              :unquote :syntax-quote :unquote-splicing
              :deref :var :fn :char}
     :atom1st- #{{\a \z \A \Z \0 \9} (any-of "!$%&*+-./:<=>?_")}
-    :atom (token :atom1st #{:atom1st \#}* (?! #{:atom1st \#}))
-    :string (token \" #{(none-of \\ \") [\\ any-char]}* \")
+    :atom (token :atom1st #{:atom1st \#}:* (?! #{:atom1st \#}))
+    :string (token \" #{(none-of \\ \") [\\ any-char]}:* \")
     :hex- {\0 \9 \a \f \A \F}
     :char (token \\ #{any-char "newline" "space" "tab" "backspace" 
                       "formfeed" "return" 
@@ -21,7 +21,7 @@
                       [\o {\0 \7} {\0 \7}]
                       [\o {\0 \3} {\0 \7} {\0 \7}]}
             (?! #{:atom1st \#}))
-    :regex (token \# \" #{(none-of \\ \") [\\ any-char]}* \") 
+    :regex (token \# \" #{(none-of \\ \") [\\ any-char]}:* \") 
     :list ["(" :expr* ")"]
     :vector ["[" :expr* "]"]
     :set ["#{" :expr* "}"]
@@ -38,9 +38,9 @@
     :var ["#'" :expr]
     :fn ["#(" :expr* ")"]
 
-    :comment (token #{"#!" ";"} (none-of \newline)* (?! (none-of \newline)))
+    :comment (token #{"#!" ";"} (none-of \newline):* (?! (none-of \newline)))
     
-    :whitespace (token #{\space \tab \newline \,}+ (?! #{\space \tab \newline \,}))))
+    :whitespace (token #{\space \tab \newline \,}:+ (?! #{\space \tab \newline \,}))))
 
 (defn step-> [& chunks]
   (let [pieces (reductions sexp nil (concat chunks [nil]))]
