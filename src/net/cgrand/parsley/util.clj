4eedca88e3701c2d5bbd119c9baa772dd3d1aa09
Christophe Grand <christophe@MacBook-Air-de-Christophe-Grand.local>
2011-06-18 21:06:09 +0200

    More variations on if-let, when-let and cond

--- src/net/cgrand/parsley/util.clj
+++ src/net/cgrand/parsley/util.clj
@@ -1,17 +1,36 @@
 (ns net.cgrand.parsley.util
-  (:refer-clojure :exclude [cond]))
+  (:refer-clojure :exclude [cond when-let if-let]))
+
+(defmacro if-let
+  "A variation on if-let where all the eprs in the bindings vector must be true."
+  ([bindings then]
+    `(if-let ~bindings ~then nil))
+  ([bindings then else]
+    (if (seq bindings)
+      `(let [test# ~(bindings 1)]
+         (if test#
+           (let [~(bindings 0) test#]
+             (if-let ~(subvec bindings 2) ~then ~else))))
+      then)))
+
+(defmacro when-let
+  "A variation on when-let where all the eprs in the bindings vector must be true."
+  [bindings & body]
+  `(if-let ~bindings (do ~@body)))
 
 (defmacro cond 
-  "A variation on cond which sports let bindings:
+  "A variation on cond which sports let bindings and implicit else:
      (cond 
        (odd? a) 1
        :let [a (quot a 2)]
        (odd? a) 2
-       :else 3)" 
+       3)" 
   [& clauses]
-  (when-let [[test expr & clauses] (seq clauses)]
-    (if (= :let test)
-      `(let ~expr (cond ~@clauses))
-      `(if ~test ~expr (cond ~@clauses)))))
+  (when-let [[test expr & more-clauses] (seq clauses)]
+    (if (next clauses)
+      (if (= :let test)
+        `(let ~expr (cond ~@more-clauses))
+        `(if ~test ~expr (cond ~@more-clauses)))
+      test)))
 
 
