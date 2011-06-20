30d30d4d59486d9af52ac0325bd184c70efe0b83
Christophe Grand <christophe@cgrand.net>
2011-06-20 19:31:39 +0200

    more abuse

--- src/net/cgrand/parsley/lr_plus.clj
+++ src/net/cgrand/parsley/lr_plus.clj
@@ -64,8 +64,7 @@
         (< cp (int 128))
           (when-let [tm (nth ascii-dispatch cp)]
             (match tm s eof))
-        :else
-          (match tm s eof))))
+        (match tm s eof))))
 
 (defn match-prefix? [token-matcher ^String s]
   (when-let [[n] (match token-matcher s false)]
@@ -101,29 +100,27 @@
   (let [[stack _ rem events] state
         s (if (= "" rem) s (str rem s))]
     (loop [stack (transient (or stack [::S])) events events s s wm (count stack)]
-      (when-let [cs (table (my-peek stack))]
-        (u/cond 
+      (u/cond
+        :when-let [cs (table (my-peek stack))]
           (and (empty? s) (:accept? cs))
-            [(persistent! stack) (dec wm) "" events]
-          :let [action (:reduce cs)]
-          action
+        [(persistent! stack) (dec wm) "" events]
+          [action (:reduce cs)]
             (let [[sym n] action
                   stack (popN! stack n)
                   cs (table (my-peek stack))
                   wm (Math/min wm (count stack))]
               (recur (conj! stack ((:gotos cs) sym)) (conj events action) s wm))
-          :else
-            (when-let [tm (:token-matcher cs)]
-              (if-let [[n id] (match tm s eof)]
-                (if (neg? n)
-                  [(persistent! stack) (dec wm) s events]
-                  (let [token (subs s 0 n)
-                        s (subs s n)
-                        wm (Math/min wm (count stack))]
-                    (recur (conj! stack ((:shifts cs) id)) (conj events token) s wm)))
-                (when-not (empty? s) 
-                  (recur stack (conj events (f/make-unexpected (subs s 0 1)))
-                         (subs s 1) wm)))))))))
+          :when-let [tm (:token-matcher cs)]
+          [[n id] (match tm s eof)]
+            (if (neg? n)
+                [(persistent! stack) (dec wm) s events]
+                (let [token (subs s 0 n)
+                      s (subs s n)
+                      wm (Math/min wm (count stack))]
+                  (recur (conj! stack ((:shifts cs) id)) (conj events token) s wm)))
+          (when-not (empty? s) 
+            (recur stack (conj events (f/make-unexpected (subs s 0 1)))
+                   (subs s 1) wm))))))
 
 (def zero [[[::S] ""] 0 f/empty-folding-queue nil])
 
@@ -170,8 +167,7 @@
       (throw (Exception. (apply str "at state " state "\n  reduce/reduce conflict " (interpose "\n" reduces))))
     (and reduction (seq shifts))
       (throw (Exception. (str "at state " state "\n shift/reduce conflict " shifts "\n" reduces)))
-    :else
-      (table-state (matcher (keys shifts)) shifts reduction gotos accept?)))
+    (table-state (matcher (keys shifts)) shifts reduction gotos accept?)))
 
 (defn to-states [{:keys [gotos shifts]}]
   (concat (vals gotos) (vals shifts)))
