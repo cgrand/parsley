b9eebcf3d49929361d9e8c72f3330874252dfab8
Christophe Grand <christophe@cgrand.net>
2012-05-29 08:44:48 +0200

    version bump to 0.9

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject net.cgrand/parsley "0.8.1"
+(defproject net.cgrand/parsley "0.9.0"
   :description "a generator of total and truly incremental parsers"
   :url "http://github.com/cgrand/parsley/"
   :dependencies [[org.clojure/clojure "[1.2.0,)"]
-                 [net.cgrand/regex "1.0.1"]])
+                 [net.cgrand/regex "1.1.0"]])
 
