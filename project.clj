1262205cae9c78118c07125af5958cba42fc66e6
Christophe Grand <christophe@cgrand.net>
2012-06-01 09:32:58 +0200

    version 0.9.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.cgrand/parsley "0.9.0"
+(defproject net.cgrand/parsley "0.9.1"
   :description "a generator of total and truly incremental parsers"
   :url "http://github.com/cgrand/parsley/"
   :dependencies [[org.clojure/clojure "[1.2.0,)"]
