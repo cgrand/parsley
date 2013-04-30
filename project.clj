3646e229ea3bd48107f53e4c1c09d8f889aad77e
Christophe Grand <christophe@cgrand.net>
2013-04-30 17:43:16 +0200

    releasing 0.9.2 for functional trees

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject net.cgrand/parsley "0.9.1"
+(defproject net.cgrand/parsley "0.9.2"
   :description "a generator of total and truly incremental parsers"
   :url "http://github.com/cgrand/parsley/"
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.cgrand/regex "1.1.0"]])
 
