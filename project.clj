2961a89259ef097d9e1b98a5e2cea8351478fc15
John Wiseman <jjwiseman@gmail.com>
2014-05-16 14:30:46 -0700

    Added :license to project.clj.
    This makes it easier to audit licenses with tools like lein licenses.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject net.cgrand/parsley "0.9.2"
   :description "a generator of total and truly incremental parsers"
   :url "http://github.com/cgrand/parsley/"
+  :license {:name "Eclipse Public License 1.0"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.cgrand/regex "1.1.0"]])
 
