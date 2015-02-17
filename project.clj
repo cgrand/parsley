b9273e28867979385c0f1eb4f68ffb9f45367817
Christophe Grand <christophe@cgrand.net>
2015-02-17 06:41:24 +0100

    version bump for 1.7 compat

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.cgrand/parsley "0.9.2"
+(defproject net.cgrand/parsley "0.9.3"
   :description "a generator of total and truly incremental parsers"
   :url "http://github.com/cgrand/parsley/"
   :license {:name "Eclipse Public License 1.0"}
